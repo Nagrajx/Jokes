94c4b6be223cd24fb4a6c675a1eeb4ac72de9f56
Satish Nagraj <132422926+Nagrajx@users.noreply.github.com>
2023-08-27 11:51:53 +0530

    Add files via upload
    THIS PAGE IS  FOR READ A JOKES AND I MADE THIS USING HTML , CSS AND JAVASCRIPT

---
+++ script.js
@@ -0,0 +1,487 @@
+let myjokes = [
+  {
+    category: "Programming",
+    type: "single",
+    joke: 'Judge: "I sentence you to the maximum punishment..."\nMe (thinking): "Please be death, please be death..."\nJudge: "Learn Java!"\nMe: "Damn."',
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 45,
+    safe: true,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: 'A man is smoking a cigarette and blowing smoke rings into the air. His girlfriend becomes irritated with the smoke and says "Can\'t you see the warning on the cigarette pack? Smoking is hazardous to your health!" to which the man replies, "I am a programmer.  We don\'t worry about warnings; we only worry about errors."',
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 38,
+    safe: true,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: "I'd tell you a joke about NAT but I would have to translate.",
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    safe: true,
+    id: 319,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: "Hey Girl,\nRoses are #ff0000,\nViolets are #0000ff,\nI use hex codes,\nBut I'd use RGB for you.",
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 41,
+    safe: true,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: "Java is like Alzheimer's, it starts off slow, but eventually, your memory is gone.",
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 27,
+    safe: false,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: "Saying that Java is nice because it works on every OS is like saying that anal sex is nice because it works on every gender.",
+    flags: {
+      nsfw: true,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: true,
+    },
+    id: 30,
+    safe: false,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: "The generation of random numbers is too important to be left to chance.",
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 39,
+    safe: true,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: 'Today I learned that changing random stuff until your program works is "hacky" and a "bad coding practice" but if you do it fast enough it\'s "Machine Learning" and pays 4x your current salary.',
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 33,
+    safe: true,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: "Debugging: Removing the needles from the haystack.",
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 40,
+    safe: true,
+    lang: "en",
+  },
+  {
+    category: "Programming",
+    type: "single",
+    joke: 'Two SQL tables sit at the bar. A query approaches and asks "Can I join you?"',
+    flags: {
+      nsfw: false,
+      religious: false,
+      political: false,
+      racist: false,
+      sexist: false,
+      explicit: false,
+    },
+    id: 221,
+    safe: true,
+    lang: "en",
+  },
+  {
+    "category": "Pun",
+    "type": "single",
+    "joke": "I bought some shoes from a drug dealer. I don't know what he laced them with, but I was tripping all day!",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 204,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "Java and C were telling jokes. It was C's turn, so he writes something on the wall, points to it and says \"Do you get the reference?\" But Java didn't.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 4,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "\"Honey, go to the store and buy some eggs.\"\n\"OK.\"\n\"Oh and while you're there, get some milk.\"\nHe never returned.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 18,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Dark",
+    "type": "single",
+    "joke": "My little daughter came to me all excited, saying \"Daddy! Daddy! Guess how old I'll be in June!\"\n\"Oh I don't know princess, why don't you tell me?\" I said. She gave me a huge smile and held up four fingers.\nIt's now three hours later, police have joined in and she still won't say where she got them.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 165,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "Your momma is so fat, you need to switch to NTFS to store a picture of her.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": true
+    },
+    "id": 55,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "The generation of random numbers is too important to be left to chance.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 39,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Dark",
+    "type": "single",
+    "joke": "I have a fish that can breakdance! Only for 20 seconds though, and only once.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 237,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "A guy walks into a bar and asks for 1.4 root beers.\nThe bartender says \"I'll have to charge you extra, that's a root beer float\".\nThe guy says \"In that case, better make it a double.\"",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 2,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "ASCII silly question, get a silly ANSI.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 25,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "A programmer puts two glasses on his bedside table before going to sleep.\nA full one, in case he gets thirsty, and an empty one, in case he doesn't.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 3,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Misc",
+    "type": "single",
+    "joke": "Relationship Status: just tried to reach for my dog's paw and he pulled it away so I pretended I was reaching for the remote.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "racist": false,
+        "sexist": false,
+        "political": false,
+        "explicit": false
+    },
+    "id": 195,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Misc",
+    "type": "single",
+    "joke": "A neutron walks into a bar and asks for a price on a drink.\nThe barkeeper says: \"For you... no charge!\"",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 73,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Misc",
+    "type": "single",
+    "joke": "I went to the zoo the other day. There was only a dog in it – it was a shihtzu.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": true
+    },
+    "safe": false,
+    "id": 307,
+    "lang": "en"
+},
+{
+    "category": "Misc",
+    "type": "single",
+    "joke": "My husband and I were happy for 20 years. And then we met.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "safe": true,
+    "id": 273,
+    "lang": "en"
+},
+{
+    "category": "Misc",
+    "type": "single",
+    "joke": "My wife and I have reached the difficult decision that we do not want children.\nIf anybody does, please just send me your contact details and we can drop them off tomorrow.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 257,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Misc",
+    "type": "single",
+    "joke": "I visited my friend at his new house. He told me to make myself at home. So I threw him out. I hate having visitors.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "safe": true,
+    "id": 304,
+    "lang": "en"
+},
+{
+    "category": "Misc",
+    "type": "single",
+    "joke": "In Soviet Russia, gay sex gets you arrested. In America, getting arrested gets you gay sex.",
+    "flags": {
+        "nsfw": true,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": true
+    },
+    "safe": false,
+    "id": 289,
+    "lang": "en"
+},
+{
+    "category": "Misc",
+    "type": "single",
+    "joke": "Yo mama is so old, she knew Burger King while he was still a prince.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "safe": true,
+    "id": 306,
+    "lang": "en"
+},
+{
+    "category": "Misc",
+    "type": "single",
+    "joke": "Women are like KFC, once you're done with the breasts and thighs, you just have a greasy box to put your bone in.",
+    "flags": {
+        "nsfw": true,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": true,
+        "explicit": true
+    },
+    "id": 149,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Misc",
+    "type": "single",
+    "joke": "Schrödinger's cat walks into a bar and doesn't.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 197,
+    "safe": true,
+    "lang": "en"
+}
+];
+ 
+
+let index = Math.floor(Math.random()*(myjokes.length -1))
+console.log(index)
+joke.innerHTML = myjokes[index].joke
